91a3c84eedd00de358833095b590ab9d0331c548
Tratsiuk Artsiom <inqui05@gmail.com>
2021-09-16 22:38:38 +0300

    fix: change menu in header

--- src/app/core/components/header.component.ts
+++ src/app/core/components/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { GetDataService } from 'src/app/shop/services/get-data.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeaderComponent {
+  constructor(public service: GetDataService) { }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header.component';
 import { LocationComponent } from './components/location/location.component';
 import { SearchComponent } from './components/search/search.component';
-import { MaterialModule } from './material/material.module';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { FooterComponent } from './components/footer/footer.component';
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
+    RouterModule,
   ],
   exports: [
     HeaderComponent,
